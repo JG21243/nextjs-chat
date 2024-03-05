d9505843f943f2d52237512a06969448484cedf7
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-03-05 01:06:00 -0500

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,51 +1,54 @@
-import NextAuth, { type DefaultSession } from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-import jwt from 'jsonwebtoken';
+import NextAuth, { type DefaultSession } from 'next-auth'
+import GitHub from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google' // Import the Google provider
 
 declare module 'next-auth' {
   interface Session {
     user: {
       /** The user's id. */
-      id: string;
-    } & DefaultSession['user'];
+      id: string
+    } & DefaultSession['user']
   }
 }
 
-export default NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth
+} = NextAuth({
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    GoogleProvider({
+    GitHub,
+    Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.id = account.id;
-        token.image = account.image;
+    jwt({ token, profile }) {
+      console.log('Profile:', profile); // Log the profile
+      if (profile) {
+        console.log('Profile ID:', profile.id); // Log the profile id
+        token.id = profile.id
+        token.image = profile.avatar_url || profile.picture
       }
-      return token;
+      console.log('Token after jwt callback:', token); // Log the token after adding id and image
+      return token
     },
-    async session({ session, token }) {
+    session: ({ session, token }) => {
+      console.log('Session before callback:', session); // Log the session before the callback
+      console.log('Token before callback:', token); // Log the token before the callback
       if (session?.user && token?.id) {
-        session.user.id = String(token.id);
+        session.user.id = String(token.id)
       }
-      return session;
+      console.log('Session after callback:', session); // Log the session after the callback
+      return session
+    },
+    authorized({ auth }) {
+      return !!auth?.user // this ensures there is a logged in user for -every- request
     }
   },
   pages: {
-    signIn: '/sign-in',
+    signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
   }
-});
+})
+
