02ecfa4164a5a9cef84d6fe7fd5d36ac3b02d8c8
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-03-07 18:32:54 -0500

    Update page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,33 +1,20 @@
-export function LoginButton({
-  provider,
-  text = `Login with ${provider.charAt(0).toUpperCase() + provider.slice(1)}`,
-  showIcon = true,
-  className,
-  ...props
-}: LoginButtonProps) {
-  const [isLoading, setIsLoading] = React.useState(false);
+import { auth } from '@/auth'
+import { LoginButton } from '@/components/login-button'
+import { redirect } from 'next/navigation'
+import React, { useState } from 'react';
+
+
+export default async function SignInPage() {
+  const session = await auth()
+  // redirect to home if user is already logged in
+  if (session?.user) {
+    redirect('/')
+  }
+
   return (
-    <Button
-      variant="outline"
-      onClick={() => {
-        setIsLoading(true);
-        signIn(provider, { callbackUrl: `/` });
-      }}
-      disabled={isLoading}
-      className={cn(className)}
-      {...props}
-    >
-      {isLoading ? (
-        <IconSpinner className="mr-2 animate-spin" />
-      ) : showIcon ? (
-        provider === 'github' ? (
-          <IconGitHub className="mr-2" />
-        ) : (
-          // Increase the margin on the right side of the Google icon
-          <IconGoogle className="mr-3" /> // was "mr-2", now "mr-3" for more space
-        )
-      ) : null}
-      {text}
-    </Button>
-  );
+    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
+      <LoginButton provider="google" />
+    </div>
+  )
 }
+
