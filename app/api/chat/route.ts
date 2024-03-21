0fc47e1f77964e7044466bdecc1f30224ec23849
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-03-21 00:52:38 -0400

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -33,7 +33,7 @@ export async function POST(req: Request) {
   }
 
   const res = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4-0125-preview',
     messages,
     temperature: 0.0,
     stream: true
