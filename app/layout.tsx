79cbd426428edd58ebcca3a326a65b27f716ff7a
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-03-24 18:53:13 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ import { Header } from '@/components/header'
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: 'Next.js AI Chatbot',
+    default: 'Next.js AI Assistant',
     template: `%s - Next.js AI Chatbot`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'An AI-powered assistant built with Next.js and Vercel.',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
