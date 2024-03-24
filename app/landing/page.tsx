7e833c402976d04f8e6bff4694c0c3489846f2c5
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-03-24 18:41:42 -0400

    Create page.tsx

---
+++ app/landing/page.tsx
@@ -0,0 +1,10 @@
+// app/landing/page.tsx
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to my landing page!</h1>
+      {/* Add your landing page content */}
+    </div>
+  );
+}
