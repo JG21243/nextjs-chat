63975ae1cd946b42fa1e0d9a869b732e4e7b2741
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-06-28 19:57:48 -0400

    Create page.tsx

---
+++ app/legal-prompts/page.tsx
@@ -0,0 +1,10 @@
+import LegalPromptLibrary from '../components/legalPromptLibrary';
+
+export default function LegalPromptsPage() {
+  return (
+    <div>
+      <h1>Legal Prompts</h1>
+      <LegalPromptLibrary />
+    </div>
+  );
+}
